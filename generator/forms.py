0ad0b9d161eeb8d5b793cfbf876c455d8569d6a4
vkjnv1 <44123611+vkjnv1@users.noreply.github.com>
2020-07-06 11:47:02 +0530

    Update forms.py

--- generator/forms.py
+++ generator/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 from .models import add
 
 class UpdateForm(ModelForm):
     class Meta:
         model = add
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
