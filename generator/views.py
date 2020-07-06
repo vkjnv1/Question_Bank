0fce6e634514054ce07dfec693338e866b9b0441
vkjnv1 <44123611+vkjnv1@users.noreply.github.com>
2020-07-06 11:50:06 +0530

    Update views.py

--- generator/views.py
+++ generator/views.py
@@ -3,7 +3,8 @@ from .models import Faculty, add
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.template.response import TemplateResponse
-from .forms import UpdateForm
+from .forms import UpdateForm, CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -13,64 +14,80 @@ def about(request):
     return render(request,'generator/about.html')
 
 
-def delete(request):
-    return render(request,'generator/deleteQ.html')
-
-def update(request, pk):
-    adds = add.objects.get(questionId=pk)
+def delete(request, myid):
+    adds = add.objects.get(questionId=myid)
     form = UpdateForm(instance=adds)
+
+    if request.method == 'POST':
+        adds.delete()
+        return('/')
+    context = {'form': form}
+    return render(request, 'generator/deleteQ.html', context)
+
+def update(request, myid):
+
+    adds = add.objects.get(questionId=myid)
+    form = UpdateForm(instance=adds)
+    #context= {'form': form}
+    #print(adds)
+    #form = UpdateForm(instance=adds)
 
     if request.method == 'POST':
         form = UpdateForm(request.POST, instance=adds)
         if form.is_valid():
-            return redirect('/')
             form.save()
-
+            request.META('/generator/create/')
+            return redirect('/')
     context = {'form': form}
     return render(request, 'generator/updateQ.html', context)
 
 def generate(request):
-    return render(request,'generator/generateQ.html')
+    return render(request, 'generator/generateQ.html')
 
 def login(request):
-    if request.method == "POST":
-        print(request)
-    return render(request, 'generator/login.html')
+    context = {}
+    return render(request, 'generator/login.html', context)
 
 def handleSignup(request):
-    if request.method == 'POST':
-        username=request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        course = request.POST['course']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
+    form = CreateUserForm()
+
+    if request.method=="POST":
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+
+    #if request.method == 'POST':
+        #username = request.POST['username']
+        #fname = request.POST['fname']
+        #lname = request.POST['lname']
+        #email = request.POST['email']
+        #subject = request.POST['subject']
+        #course = request.POST['course']
+        #pass1 = request.POST['pass1']
+        #pass2 = request.POST['pass2']
 
         #check for errorneous inputs
 
         # create the user
 
-        myuser = User.objects.create_user(username,email,pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-        myuser.save()
-        messages.success(request,"Your account has been successfully created")
+        #myuser = User.objects.create_user(username,email,pass1)
+        #myuser.first_name = fname
+        #myuser.last_name = lname
+        #myuser.save()
+        #messages.success(request,"Your account has been successfully created")
         # return redirect('generator/addQ')
-        return HttpResponse('generator/signup')
+        #return HttpResponse('generator/signup')
 
-    else :
-        return HttpResponse('404 - not found')
-
-
-
+    #else :
+        #return HttpResponse('404 - not found')
+    return render(request, 'generator/signup.html', context)
 
 def addQ(request):
     # adds = add.objects.all(id=pk_test)
 
     if request.method == 'POST':
-        questionId=request.POST.get('questionId', '')
+        # questionId=request.POST.get('questionId', '')
         EnterTheQuestion = request.POST.get('EnterTheQuestion', '')
         Subject = request.POST.get('Subject', '')
         DifficultLevel = request.POST.get('DifficultLevel', '')
@@ -81,8 +98,8 @@ def addQ(request):
         Semester = request.POST.get('Semester', '')
         Subject_code = request.POST.get('Subject_code', '')
 
-        addition=add(questionId=questionId, EnterTheQuestion=EnterTheQuestion, DifficultLevel=DifficultLevel, unitNo=unitNo, Subject=Subject, year=year, Marks_alloted=Marks_alloted,
-                 Module=Module, Semester=Semester, Subject_code=Subject_code)
+        addition = add(EnterTheQuestion = EnterTheQuestion, DifficultLevel = DifficultLevel, unitNo = unitNo, Subject = Subject, year = year, Marks_alloted = Marks_alloted,
+                 Module = Module, Semester = Semester, Subject_code = Subject_code)
         addition.save()
     return render(request, 'generator/addQ.html')
 
@@ -90,4 +107,5 @@ def create(request):
     adds = add.objects.all()
     context = {'adds': adds}
     return render(request, 'generator/create.html', context)
+
 
